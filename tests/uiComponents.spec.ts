93621c573045c9630e6f046ce34507cb4ae4b1d2
Donald Cuka <donaldcuka@gmail.com>
2024-04-22 15:21:00 +0200

    Renaming

--- tests/uiComponents.spec.ts
+++
@@ -1,9 +0,0 @@
-import {test, expect} from '@playwright/test'
-
-test.beforeEach (async({page}) => {
-    await page.goto('https://shop.polymer-project.org/')
-})
-
-test('navigate to Mens Outerwear page', async ({page}) => {
-    
-})
